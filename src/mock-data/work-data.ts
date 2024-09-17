673df40e69e3939c55d5b27ccee90ebf815bd7f5
Aman <aman.verma@thegatewaycorp.co.in>
2024-09-17 11:06:04 +0530

    implementing skill and work

---
+++ src/mock-data/work-data.ts
@@ -0,0 +1,37 @@
+export interface workDataType {
+  title: string;
+  description: string;
+}
+
+export const workData: workDataType[] = [
+  {
+    title: "Frontend",
+    description:
+      "Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+  {
+    title: "Backend",
+    description:
+      "Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+  {
+    title: "Deployment",
+    description:
+      "Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+  {
+    title: "Mobile Application",
+    description:
+      " Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+  {
+    title: "Wireframe",
+    description:
+      " Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+  {
+    title: "Desktop Application",
+    description:
+      " Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.",
+  },
+];
--- src/pages/portfolio-pages/page.tsx
+++ src/pages/portfolio-pages/page.tsx
@@ -1,84 +1,9 @@
 import CustomNavBarComponent from "../../components/nav-bar-component/component";
-import react_logo from "../../assets/images/react_logo.png";
-import next_logo from "../../assets/images/next_logo.png";
-import node_logo from "../../assets/images/nodejs_logo.png";
-import typescript_logo from "../../assets/images/typescript_logo.png";
-import mongodb_logo from "../../assets/images/mongodb_logo.png";
-import mern_logo from "../../assets/images/mern_logo.png";
-import redux_logo from "../../assets/images/redux_logo.png";
-import docker_logo from "../../assets/images/docker_logo.png";
-import html5_logo from "../../assets/images/html5_logo.png";
-import css_logo from "../../assets/images/css_logo.png";
-import tailwind_logo from "../../assets/images/tailwind_logo.png";
-import python_logo from "../../assets/images/python_logo.png";
-import shellscript_logo from "../../assets/images/shellscript_logo.png";
-import kalilinux_logo from "../../assets/images/kalilinux_logo.png";
 import "../../css/scroll-container.css";
 import "../../css/sliding_animation.css";
-
-interface skillsType {
-  title: string;
-  logo: string;
-}
-
-const skillsData: skillsType[] = [
-  {
-    title: "React JS",
-    logo: react_logo,
-  },
-  {
-    title: "Next JS",
-    logo: next_logo,
-  },
-  {
-    title: "Redux",
-    logo: redux_logo,
-  },
-  {
-    title: "Typescript",
-    logo: typescript_logo,
-  },
-  {
-    title: "Node JS",
-    logo: node_logo,
-  },
-  {
-    title: "MongoDB",
-    logo: mongodb_logo,
-  },
-  {
-    title: "MERN STACK",
-    logo: mern_logo,
-  },
-  {
-    title: "Docker",
-    logo: docker_logo,
-  },
-  {
-    title: "HTML5",
-    logo: html5_logo,
-  },
-  {
-    title: "CSS",
-    logo: css_logo,
-  },
-  {
-    title: "Tailwind CSS",
-    logo: tailwind_logo,
-  },
-  {
-    title: "Python",
-    logo: python_logo,
-  },
-  {
-    title: "Shell Scripting",
-    logo: shellscript_logo,
-  },
-  {
-    title: "Kali Linux",
-    logo: kalilinux_logo,
-  },
-];
+import { MoveUpRight } from "lucide-react";
+import { skillsData } from "../../mock-data/skill-data";
+import { workData } from "../../mock-data/work-data";
 
 const PortfolioPage = () => {
   return (
@@ -86,21 +11,21 @@ const PortfolioPage = () => {
       <div>
         <CustomNavBarComponent />
       </div>
-      <div className="w-full h-[50rem] p-4 flex flex-col items-center justify-end gap-4">
+      <div className="w-full h-full p-4 flex flex-col items-center justify-start pt-[20rem] gap-4 scroll-container">
         <div className="w-full md:w-[25.5%]">
           <p className="text-5xl md:text-8xl text-[#DAC5A7] text-center">
             Web Designer & Developer
           </p>
         </div>
+
         <div>
           <p className="text-xs text-[#DAC5A7] text-opacity-60 text-center font-display">
             Premium web design, development, and SEO services to help your
             business stand out.
           </p>
         </div>
-        <div
-          className="p-2 w-full flex items-center justify-center gap-[5rem] scroll-container"
-        >
+
+        <div className="p-2 w-full flex items-center justify-center gap-[5rem]">
           {skillsData.map((skill, index) => {
             return (
               <div className="flex items-center justify-center gap-2 sliding_animation select-none">
@@ -108,6 +33,39 @@ const PortfolioPage = () => {
                 <p className="text-xs font-display text-[#DAC5A7] text-nowrap">
                   {skill.title}
                 </p>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="w-full p-4 flex flex-wrap items-center justify-center gap-4">
+          {workData.map((work, index) => {
+            return (
+              <div className="w-[30rem] border-2 border-[#DAC5A7] border-opacity-15 bg-[#DAC5A7] bg-opacity-5 rounded-lg p-10 animate-slideUp">
+                <div>
+                  <p className="text-xs font-display text-[#DAC5A7] text-opacity-60">
+                    {`0${index + 1}`}
+                  </p>
+                </div>
+                <div className="mt-5">
+                  <p className="text-4xl font-light font-display text-[#DAC5A7] uppercase">
+                    {work.title}
+                  </p>
+                </div>
+                <div className="mt-5">
+                  <p className="font-display text-[#DAC5A7] text-opacity-60 font-light">
+                    Visually stunning web designs that captivate your audience
+                    by blending your brand voice and customer needs.
+                  </p>
+                </div>
+                <div className="mt-5 flex items-center gap-4">
+                  <button className="border-2 border-[#DAC5A7] border-opacity-15 bg-[#DAC5A7] bg-opacity-5 p-3 rounded-full">
+                    <MoveUpRight className="text-[#DAC5A7] w-5 h-5" />
+                  </button>
+                  <p className="text-2xl font-light font-display text-[#DAC5A7] uppercase">
+                    {`About ${work.title}`}
+                  </p>
+                </div>
               </div>
             );
           })}
