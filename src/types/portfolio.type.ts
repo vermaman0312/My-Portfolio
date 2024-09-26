6876b7a4920649e615b12cd690831eb1e094f013
Aman <aman.verma@thegatewaycorp.co.in>
2024-09-26 19:26:11 +0530

    updated code ...

--- src/types/portfolio.type.ts
+++ src/types/portfolio.type.ts
@@ -1,74 +1,76 @@
+import React from "react";
+
 export type TUserSocialLinksType = {
-  title: string;
-  icon: string;
-  url: string;
+  title: string | null;
+  icon: React.ReactNode | null;
+  url: string | null;
 };
 
 export type TUserEducationType = {
-  faculty: string;
-  branch: string;
-  institute: string;
-  yearOfPassing: string;
-  percentage: string;
+  faculty: string | null;
+  branch: string | null;
+  institute: string | null;
+  yearOfPassing: string | null;
+  percentage: string | null;
 };
 
 export type TUserCertificationsType = {
-  certificationName: string;
-  certificationDescription: string;
-  certificationAuthority: string;
-  certificationDate: string;
-  certificationLogo: string;
-  certificationImage: string;
-  certificationBadge: string;
+  certificationName: string | null;
+  certificationDescription: string | null;
+  certificationAuthority: string | null;
+  certificationDate: string | null;
+  certificationLogo: string | null;
+  certificationImage: string | null;
+  certificationBadge: string | null;
 };
 
 export type TUserWorkExperienceType = {
-  companyName: string;
-  designation: string;
-  yearOfJoining: string;
-  yearOfLeaving: string;
+  companyName: string | null;
+  designation: string | null;
+  yearOfJoining: string | null;
+  yearOfLeaving: string | null;
 };
 
 export type TUserSkillsType = {
-  skillName: string;
-  skillLevel: string;
-  skillShortDescription: string;
-  skillLongDescription: string;
-  skillLogo: string;
+  skillName: string | null;
+  skillLevel: string | null;
+  skillShortDescription: string | null;
+  skillLongDescription: string | null;
+  skillLogo: string | null;
 };
 
 export type TUserServicesWorkType = {
-  type: string;
-  title: string;
-  description: string;
-  icons: Array<string>;
-  concept: string;
-  design: string;
-  protoType: string;
+  type: string | null;
+  title: string | null;
+  description: string | null;
+  icons: Array<string> | null;
+  concept: string | null;
+  design: string | null;
+  protoType: string | null;
 };
 
 export type TUserProjectsType = {
-  projectName: string;
-  projectDescription: string;
-  projectTechnologies: Array<string>;
-  projectImageURL: Array<string>;
-  projectLinkURL: Array<string>;
+  projectName: string | null;
+  projectDescription: string | null;
+  projectTechnologies: Array<string> | null;
+  projectImageURL: Array<string> | null;
+  projectLinkURL: Array<string> | null;
 };
 
 export type TUserPortfolioTypeInterface = {
-  userName: string;
-  userEmailAdd: string;
-  userPhoneNum: string;
-  userAddress: string;
-  userPinCode: string;
-  userObjectiv1: string;
-  userObjectiv2: string;
-  userObjectiv3: string;
-  userSocialLinks: Array<TUserSocialLinksType>;
-  userEducation: Array<TUserEducationType>;
-  userCertifications: Array<TUserCertificationsType>;
-  userWorkExperience: Array<TUserWorkExperienceType>;
-  userSkills: Array<TUserSkillsType>;
-  userServicesWork: Array<TUserServicesWorkType>;
-  userProjects: Array<TUserProjectsType>;
+  userName: string | null;
+  userEmailAddress: string | null;
+  userPhoneNumber: string | null;
+  userAddress: string | null;
+  userPinCode: string | null;
+  userObjective1: string | null;
+  userObjective2: string | null;
+  userObjective3: string | null;
+  userSocialLinks: Array<TUserSocialLinksType> | null;
+  userEducation: Array<TUserEducationType> | null;
+  userCertifications: Array<TUserCertificationsType> | null;
+  userWorkExperience: Array<TUserWorkExperienceType> | null;
+  userSkills: Array<TUserSkillsType> | null;
+  userServicesWork: Array<TUserServicesWorkType> | null;
+  userProjects: Array<TUserProjectsType> | null;
 };
