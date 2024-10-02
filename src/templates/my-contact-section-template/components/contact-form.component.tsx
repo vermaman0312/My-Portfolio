23453f1a11d2a202742937b529a99b8be42a45db
Aman <aman.verma@thegatewaycorp.co.in>
2024-10-02 11:53:23 +0530

    updated code .... form

--- src/templates/my-contact-section-template/components/contact-form.component.tsx
+++ src/templates/my-contact-section-template/components/contact-form.component.tsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { CustomLabel } from "../../../components/custom-components/custom-label/component";
 import { Button } from "../../../components/ui/button-ui/ui";
+import emailjs from "emailjs-com";
+
+interface formData {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
 
 const PortfolioContactFormPageComponent = () => {
+  const [formData, setFormData] = useState<formData>({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    },
+    [formData]
+  );
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Replace with your EmailJS user ID and service/template IDs
+    const serviceID = "service_cmequjq";
+    const templateID = "template_c5wri3h";
+    const userID = "4PtK9Y-6kWjt0Uc6R";
+
+    emailjs
+      .send(
+        serviceID,
+        templateID,
+        Object.fromEntries(Object.entries(formData)),
+        userID
+      )
+      .then(
+        (response) => {
+          console.log("Email sent successfully:", response);
+          setFormData({ name: "", email: "", phone: "", message: "" });
+        },
+        (error) => {
+          console.error("Error sending email:", error);
+        }
+      );
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-2 md:p-16 p-4">
       <div className="w-full flex items-center justify-center">
@@ -14,6 +63,9 @@ const PortfolioContactFormPageComponent = () => {
         <CustomLabel className="font-display text-[#DAC5A7]">Name:</CustomLabel>
         <input
           type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
           placeholder="Your name"
           className="border-2 border-[#716a60] outline-none text-xs text-[#DAC5A7] text-opacity-70 placeholder:text-[#DAC5A7] placeholder:text-opacity-40 font-display bg-transparent w-full p-2 rounded-lg"
         />
@@ -22,6 +74,9 @@ const PortfolioContactFormPageComponent = () => {
         <CustomLabel className="font-display">Email address:</CustomLabel>
         <input
           type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
           placeholder="example@example.com"
           className="border-2 border-[#716a60] outline-none text-xs text-[#DAC5A7] text-opacity-70 placeholder:text-[#DAC5A7] placeholder:text-opacity-40 font-display bg-transparent w-full p-2 rounded-lg"
         />
@@ -30,6 +85,9 @@ const PortfolioContactFormPageComponent = () => {
         <CustomLabel className="font-display">Contact No:</CustomLabel>
         <input
           type="text"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
           placeholder="+XX-XXXXXXXXXX"
           className="border-2 border-[#716a60] outline-none text-xs text-[#DAC5A7] text-opacity-70 placeholder:text-[#DAC5A7] placeholder:text-opacity-40 font-display bg-transparent w-full p-2 rounded-lg"
         />
@@ -38,12 +96,18 @@ const PortfolioContactFormPageComponent = () => {
         <CustomLabel className="font-display">Message:</CustomLabel>
         <textarea
           placeholder="write a message..."
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
           className="border-2 border-[#716a60] outline-none text-xs text-[#DAC5A7] text-opacity-70 placeholder:text-[#DAC5A7] placeholder:text-opacity-40 font-display bg-transparent w-full p-2 min-h-24 rounded-lg max-h-32 scroll-container"
         />
       </div>
 
       <div className="w-full flex items-center justify-center">
-        <Button className="bg-[#DAC5A7] text-black w-32 h-10 font-display">
+        <Button
+          onClick={handleSubmit}
+          className="bg-[#DAC5A7] text-black w-32 h-10 font-display"
+        >
           Reach Me
         </Button>
       </div>
